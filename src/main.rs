755187275caf81b76025bfcae0f69bb7e27c053a
Marcos Felipe <marcosfbdcarvalho@gmail.com>
2021-12-11 12:01:50 -0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,45 @@
-use std::io::{stdin, stdout, Write};
+use std::io::{stdin};
 
-mod fifth_choice;
-use fifth_choice::fifth_choice;
+fn date_between_the_last_inferior_conjunction(){
 
-mod fourth_choice;
-use fourth_choice::fourth_choice;
+}
 
-mod third_choice;
-use third_choice::third_choice;
+fn angle_venus_sun_earth(){
 
-mod second_choice;
-use second_choice::second_choice;
+}
 
-mod first_choice;
-use first_choice::first_choice;
+fn angle_sun_venus_earth(){
 
-fn main() {
-    
-    let mut information = String::new();
-    print!("What information do you have?: 
-    \n 1) Earth-Venus distance.
-    \n 2) Sol-Earth-Venus angle.
-    \n 3) Sol-Venus-Earth angle.
-    \n 2) Venus-Sol-Terra angle.
-    \n 5) Date - Between the last inferior conjunction (2020-06-03) and the next (2022-01-08)
-    \n Just inform the correspondent number: ");
-    let _error = stdout().flush();
+}
+
+fn angle_sun_earth_venus(){
+
+}
+
+fn distance_earth_venus(){
+    // let distance_earth_venus: f64 = 
+}
+
+fn main(){
+
+    let mut information: String = String::new();
+    println!("Wich information do you have? \n 1) distance Earth-Venus \n 2) angle Sun-Earth-Venus \n 3) angle Sun-Venus-Earth \n 4) angle Venus-Sun-Earth \n 5) date between the last inferior conjunction (2020-06-03) and the next (2022-01-08) \n Input the corresponding number: ");
     stdin().read_line(&mut information).expect("Did not enter a correct string!");
+    let option: u8 = information.trim().parse().expect("Invalid option. Try again.");
 
-    if information == "1"{
-        first_choice();
-    }else if information == "2"{
-        second_choice();
-    }else if information == "3"{
-        third_choice();
-    }else if information == "4"{
-        fourth_choice();
+    if option == 1{
+        distance_earth_venus();
+    }else if option == 2{
+        angle_sun_earth_venus();
+    }else if option == 3{
+        angle_sun_venus_earth();
+    }else if option == 4{
+        angle_venus_sun_earth();
+    }else if option == 5{
+        date_between_the_last_inferior_conjunction();
     }else{
-        fifth_choice();
+        println!("Invalid option. Try again.");
+        main();
     }
 
 }
