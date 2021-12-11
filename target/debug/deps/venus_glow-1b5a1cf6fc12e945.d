6cb97147f73cb112e4215d91334f55510888bd3c
Marcos Felipe <marcosfbdcarvalho@gmail.com>
2021-12-11 11:56:42 -0300

    Create venus_glow-1b5a1cf6fc12e945.d

---
+++ target/debug/deps/venus_glow-1b5a1cf6fc12e945.d
@@ -0,0 +1,5 @@
+c:\Projetos\VenusGlow\target\debug\deps\venus_glow-1b5a1cf6fc12e945.rmeta: src\main.rs
+
+c:\Projetos\VenusGlow\target\debug\deps\venus_glow-1b5a1cf6fc12e945.d: src\main.rs
+
+src\main.rs:
