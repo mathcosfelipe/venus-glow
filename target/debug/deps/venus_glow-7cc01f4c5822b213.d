c9e7e1cb1fd11f9a78f54c96cc9b47c72f302da5
Marcos Felipe <marcosfbdcarvalho@gmail.com>
2021-12-11 12:59:12 -0300

    Delete venus_glow-7cc01f4c5822b213.d

--- target/debug/deps/venus_glow-7cc01f4c5822b213.d
+++
@@ -1,5 +0,0 @@
-c:\Projetos\VenusGlow\target\debug\deps\venus_glow-7cc01f4c5822b213.rmeta: src\main.rs
-
-c:\Projetos\VenusGlow\target\debug\deps\venus_glow-7cc01f4c5822b213.d: src\main.rs
-
-src\main.rs:
