453e70d65b543028ab79d53868010c35b90cd0ca
Marcos Felipe <marcosfbdcarvalho@gmail.com>
2021-12-21 20:36:57 -0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,30 @@
 use std::io::{stdin};
 
-mod math{
-    pub fn acos(){}
+mod distance_earth_venus{
+    pub fn distance_earth_venus(){}
 }
 
-use math::acos;
-
-fn date_between_the_last_inferior_conjunction(){
-
+mod angle_sun_earth_venus{
+    pub fn angle_sun_earth_venus(){}
 }
 
-fn angle_venus_sun_earth(){
-
+mod angle_sun_venus_earth{
+    pub fn angle_sun_venus_earth(){}
 }
 
-fn angle_sun_venus_earth(){
-
+mod angle_venus_sun_earth{
+    pub fn angle_venus_sun_earth(){}
 }
 
-fn angle_sun_earth_venus(){
-
+mod date_between_the_last_inferior_conjunction{
+    pub fn date_between_the_last_inferior_conjunction(){}
 }
 
-fn distance_earth_venus(){
-
-    let mut str_error: &str;
-    let mut integer_error: u8;
-    
-    let mut psi: f64 = 0.0; 
-    let mut phi: f64 = 0.0; 
-    let mut theta: f64 = 0.0; 
-    let mut tau: f64 = 0.0; 
-    let mut upsilon: f64 = 0.0; 
-    
-    const beta: f64 = 10.81;
-    const gama: f64 = 14.95;
-
-    let mut distance_earth_venus: String = String::new();
-    stdin().read_line(&mut distance_earth_venus).expect("Did not enter a correct string!");
-    let mut value_distance_earth_venus: f64 = distance_earth_venus.trim().parse().expect("Invalid option. Try again.");
-    let power: u64 = u64::pow(10, 7);
-    value_distance_earth_venus = value_distance_earth_venus / power as f64;
-
-    if value_distance_earth_venus == 25.76{
-        str_error = "conjusup";
-    }else if value_distance_earth_venus > 25.76{
-        integer_error = 2;
-    }else if value_distance_earth_venus == 4.14{
-        str_error = "conjuinf";
-    }else if value_distance_earth_venus < 4.15{
-        integer_error = 2;    
-    }else{
-        psi = acos();
-    }
-
-}
+use distance_earth_venus::distance_earth_venus;
+use angle_sun_earth_venus::angle_sun_earth_venus;
+use angle_sun_venus_earth::angle_sun_venus_earth;
+use angle_venus_sun_earth::angle_venus_sun_earth;
+use date_between_the_last_inferior_conjunction::date_between_the_last_inferior_conjunction;
 
 fn main(){
 
