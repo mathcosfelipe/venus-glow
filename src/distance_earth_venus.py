a1cfcb8fb5b7441216169d5a37a5bf30e3468c1d
Marcos Felipe <marcosfbdcarvalho@gmail.com>
2021-12-21 22:23:51 -0300

    Create distance_earth_venus.py

---
+++ src/distance_earth_venus.py
@@ -0,0 +1,16 @@
+import math
+
+def distance_earth_venus(psi, phi, theta, tau, upsilon, beta, gama):
+    
+    while True:
+        try:
+            distance_earth_venus = float(input('Input the value of distance between Earth and Venus, in Kilometers: '))
+        except:
+            print('Invalid value. Informa a float number.')
+        else:
+            break
+
+    distance_earth_venus = distance_earth_venus / 10 ** 7
+    psi = (math.acos((distance_earth_venus ** 2 + gama ** 2 - beta ** 2) / (2 * distance_earth_venus * gama)))
+    phi = (math.acos((distance_earth_venus ** 2 + beta ** 2 - gama ** 2) / (2 * distance_earth_venus * beta)))
+    theta = (math.acos((beta ** 2 + gama ** 2 - distance_earth_venus ** 2) / (2 * beta * gama)))
