d466263da0d23a7320cdf93bd8f3458ed2536d27
Marcos Felipe <marcosfbdcarvalho@gmail.com>
2022-01-22 12:46:38 -0300

    Update distance_earth_venus.py

--- src/distance_earth_venus.py
+++ src/distance_earth_venus.py
@@ -1,10 +1,36 @@
 import math
 from datetime import timedelta
+from datetime import date
 
-def distance_earth_venus(psi, phi, theta, tau, upsilon, beta, gama, date_a, date_b, last_inferior_conjunction, next_inferior_conjunction, date_superior_conjunction, inferior_conjunction_a, inferior_conjunction_b, superior_conjunction_a, superior_conjunction_b, max_conjunction_a, max_conjunction_b):
+def distance_earth_venus():
     
-    error = None
     today = None
+    error = 0
+
+    psi = 0.0 
+    phi = 0.0 
+    theta = 0.0 
+    tau = 0.0 
+    upsilon = 0.0
+
+    beta = 10.81
+    gama = 14.95
+
+    last_inferior_conjunction = date(2020, 6, 3) 
+    next_inferior_conjunction = date(2022, 1, 8)
+    date_superior_conjunction = date(2021, 3, 22)
+
+    t = 0
+
+    date_a = 0
+    date_b = 0
+
+    inferior_conjunction_a = 0
+    inferior_conjunction_b = 0
+    superior_conjunction_a = 0
+    superior_conjunction_b = 0
+    max_conjunction_a = 0
+    max_conjunction_b = 0
     
     while True:
         try:
